946605d860907f55685963de065feb279bb5d2c6
Ziad Saab <ziad.saab@gmail.com>
2022-08-22 22:00:57 -0400

    feat: initial commit

--- src/index.js
+++ src/index.js
@@ -1,18 +1,53 @@
-module.exports.onRpcRequest = async ({ origin, request }) => {
+const ensureSnapquanowIsInstalled = async () => {
+  await wallet.request({
+    method: 'wallet_enable',
+    params: [
+      {
+        wallet_snap: {
+          'local:http://localhost:8080/': {},
+        },
+      },
+    ],
+  });
+};
+
+const callSnapquanow = async (method, params) => {
+  await ensureSnapquanowIsInstalled();
+  return wallet.request({
+    method: 'wallet_invokeSnap',
+    params: [
+      'local:http://localhost:8080/',
+      {
+        method,
+        params,
+      },
+    ],
+  });
+};
+
+module.exports.onRpcRequest = async ({ request }) => {
   switch (request.method) {
-    case 'hello':
-      return wallet.request({
-        method: 'snap_confirm',
-        params: [
-          {
-            prompt: `Hello, ${origin}!`,
-            description:
-              'This custom confirmation is just for display purposes.',
-            textAreaContent:
-              'But you can edit the snap source code to make it do something, if you want to!',
-          },
-        ],
+    case 'getEthereumPrice':
+      return callSnapquanow('getTokenPrice', {
+        token: 'ETH',
+        fiat: 'USD',
       });
+
+    case 'monitorEthereumPrice': {
+      await callSnapquanow('streamTokenPrice', {
+        token: 'ETH',
+        fiat: 'USD',
+        callback: {
+          snapId: 'local:http://localhost:8888/',
+          method: 'receiveEthereumPrice',
+        },
+      });
+
+      return true;
+    }
+    case 'receiveEthereumPrice':
+      console.log('Received ETH price', request.params);
+      return false;
     default:
       throw new Error('Method not found.');
   }
